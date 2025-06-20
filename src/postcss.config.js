7f1db1bf08bcb897b6766411c9194bf99cb9ea60
Subodhit Chouhan <chouhansubodhit@gmail.com>
2025-06-21 00:06:43 +0530

    tailwindcorrect1

--- src/postcss.config.js
+++ src/postcss.config.js
@@ -1,6 +1,7 @@
+// postcss.config.js
 module.exports = {
   plugins: [
-    require('@tailwindcss/postcss')(),  // Use new plugin
+    require('@tailwindcss/postcss')(),  // Use new plugin correctly with ()
     require('autoprefixer'),
   ],
 };
